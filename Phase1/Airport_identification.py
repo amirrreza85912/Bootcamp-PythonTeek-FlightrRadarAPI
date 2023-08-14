9cf9076440eb97192c89251e423649f6af077c26
amirrreza85912 <142075216+amirrreza85912@users.noreply.github.com>
2023-08-14 11:41:46 +0330

    Update Airport_identification.py
    Complete step1

--- Phase1/Airport_identification.py
+++ Phase1/Airport_identification.py
@@ -22,18 +22,18 @@ details = airport_details['airport']['pluginData']['details']
 position = airport_details['airport']['pluginData']['details']['position']
 airportImages = airport_details['airport']['pluginData']['details']['airportImages']
 
-'''
-Step1:
 
-name = ...
-country = ...
-city = ...
+# Step1:
+
+name = details['name']
+country = position['country']['name']
+city = position['region']['city']
 
 print("{} in: {} / {}".format(name, city, country))
-'''
 
 
-'''
+
+...
 Step2:
 latitude = ...
 longitude = ...
